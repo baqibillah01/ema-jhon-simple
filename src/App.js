3e6b67474f256151f3032c7c9c6c55aaaefe96bd
baqibillah01 <baqibilla00@gmail.com>
2023-05-11 11:03:49 +0600

    add cart and router

--- src/App.js
+++ src/App.js
@@ -2,12 +2,45 @@ import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
 import Shop from './components/Shop/Shop';
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
+import Home from './components/Layout/Home';
+import Orders from './components/Orders/Orders';
+import Inventory from './components/Inventory/Inventory';
+import Login from './components/Login/Login';
+
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home></Home>,
+    children: [
+      {
+        path:'/',
+        element: <Shop></Shop>
+      },
+      {
+        path: 'orders',
+        element: <Orders></Orders>
+      },
+      {
+        path: 'inventory',
+        element: <Inventory></Inventory>
+      },
+      {
+        path: 'login',
+        element: <Login></Login>
+      }
+    ]
+  }
+])
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
-      <Shop></Shop>
+      <RouterProvider router={router} />
     </div>
   );
 }

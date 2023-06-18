18d28a30a43ef7e2338d0cbb97a5c2803d1e02d6
baqibillah01 <baqibilla00@gmail.com>
2023-06-19 00:21:54 +0600

    added singup

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Login.css";
 
 const Login = () => {
@@ -7,10 +8,16 @@ const Login = () => {
       <h1 className="form-title">Login</h1>
       <form>
         <div className="form-control">
-          <label htmlFor="">Email</label>
-          <input type="email" name="email" id="" required/>
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="" required />
         </div>
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Login" />
       </form>
+      <p><small>New to Ema-jhon? <Link to="/signup">Create New Account</Link></small></p>
     </div>
   );
 };
--- src/components/SignUp/SignUp.js
+++ src/components/SignUp/SignUp.js
@@ -1,12 +1,39 @@
-import React from 'react';
-import './SignUp.css'
+import React from "react";
+import { Link } from "react-router-dom";
+import "./SignUp.css";
 
 const SignUp = () => {
-    return (
-        <div>
-            <h2>Please Sign Up</h2>
+    const handleSignUp = event =>{
+        event.preventDefault();
+
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+        const confirm = form.confirm.value;
+        console.log(email, password, confirm);
+    }
+
+  return (
+    <div className="form-container">
+      <h1 className="form-title">Sign Up</h1>
+      <form onSubmit={handleSignUp}>
+        <div className="form-control">
+          <label htmlFor="email">Email</label>
+          <input type="email" name="email" id="" required />
         </div>
-    );
+        <div className="form-control">
+          <label htmlFor="password">Password</label>
+          <input type="password" name="password" id="" required />
+        </div>
+        <div className="form-control">
+          <label htmlFor="confirm">Confirm Password</label>
+          <input type="password" name="confirm" id="" required />
+        </div>
+        <input className="btn-submit" type="submit" value="Sign Up" />
+      </form>
+      <p><small>Already have an account? <Link to="/login">Login</Link></small></p>
+    </div>
+  );
 };
 
 export default SignUp;
